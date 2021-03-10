b5b57add797e57b985f7ec0a5d89b9fd01797657
Abby Brown <abby_brown3@baylor.edu>
2021-03-09 23:19:08 -0600

    Got level to display! yay. Moving on to number of ships

--- first_dev.py
+++ first_dev.py
@@ -103,6 +103,7 @@ class AlienInvasion:  # Starting point!
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
             # Get rid of any remaining aliens and bullets
             self.aliens.empty()
@@ -168,11 +169,16 @@ class AlienInvasion:  # Starting point!
                 self.stats.score += self.settings.alien_points * len(aliens)
                 self.sb.prep_score()
                 self.sb.check_high_score()
+
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase level
+            self.stats.level += 1
+            self.sb.prep_level()
 
     ##############################################################
     def _create_fleet(self):
