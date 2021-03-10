db9bfbce1797d3ef8028f3bf0b9bdbe917db63e4
Abby Brown <abby_brown3@baylor.edu>
2021-03-09 22:18:19 -0600

    aliens now assigned point values; moving on to resetting score

--- first_dev.py
+++ first_dev.py
@@ -163,6 +163,9 @@ class AlienInvasion:  # Starting point!
         """Respond to bullet alien collisions."""
         # Remove any bullets and aliens that have collided.
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+        if collisions:
+            self.stats.score += self.settings.alien_points
+            self.sb.prep_score()
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
